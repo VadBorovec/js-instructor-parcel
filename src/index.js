8e99e3db47b8f1bb01d5b0ea084ae3906ac26507
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-31 20:43:21 +0300

    first

--- src/index.js
+++
@@ -1,4 +0,0 @@
-// import './pages/16-localstorage/src/index';
-// import './pages/16-localstorage/src/js/03-colorpicker';
-
-import colorCardTpl from './pages/16-localstorage/src/templates/color-cards.hbs';
